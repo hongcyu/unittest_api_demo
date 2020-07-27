5902d1e2e67ae078e359c1ed1817891b0a3fa24e
hongCYu <hongcyu@qq.com>
2020-07-27 10:08:04 +0800

    edit project_path

--- utils/project_path.py
+++ utils/project_path.py
@@ -7,4 +7,20 @@ project_path.py
 '''
 import os
 
-path =os.path.split(os.path.split(os.path.realpath(__file__))[0])
+# 获取当前目录的最大路径
+project_path =os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
+
+#测试数据的路径
+if not os.path.exists(project_path + r'\data'):
+    os.mkdir(project_path + r'\data')
+test_case_path = os.path.join(project_path,'data','data_http.xlsx')
+
+#测试报告路径
+if not os.path.exists(project_path + r'\report'):
+    os.mkdir(project_path + r'\report')
+report_path = os.path.join(project_path,'report','test_http_report.html')
+
+#配置文件路径
+if not os.path.exists(project_path + r'\config'):
+    os.mkdir(project_path + r'\config')
+config_path = os.path.join(project_path,'config','http_case.config')
