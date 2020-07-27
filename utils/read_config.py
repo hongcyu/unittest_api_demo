d49052ac7f6b180801d4f5ce36e0b09f00b0c7b1
hongCYu <hongcyu@qq.com>
2020-07-27 23:27:13 +0800

    edit config

--- utils/read_config.py
+++ utils/read_config.py
@@ -2,7 +2,22 @@
 
 '''
 read_config.py
-用于读取config文件，后期计划改成读取json
+用于读取config文件的方法
 
 '''
 
+import configparser
+import project_path
+
+class Read_config(object):
+
+    @staticmethod
+    def get_config(config_path,section,option):
+        config = configparser.ConfigParser() #初始化对象
+        config.read(config_path,encoding="utf-8")#注意使用utf-8编码读取
+        return config[section][option]
+
+if __name__ == "__main__":
+    cf = Read_config.get_config(project_path.config_path,'MODE','mode')
+    print(cf)
+    
--- utils/read_excel.py
+++ utils/read_excel.py
@@ -5,3 +5,14 @@ read_excel.py
 读取excel表单内的数据，通过配置文件实现测试用例的配置
 
 '''
+
+import project_path
+from read_config import Read_config
+from openpyxl import load_workbook
+
+class Read_the_excel(object):
+
+    @staticmethod
+    def get_data(test_case_path):
+
+
